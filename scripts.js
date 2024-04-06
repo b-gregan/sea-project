10a68a09cd2644a1609b2860bbb4d939310f75c6
brenda <b.gregann@gmail.com>
2024-04-05 20:41:27 -0700

    Add Species class and getSpecies function

--- scripts.js
+++ scripts.js
@@ -1,30 +1,61 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+
+
+class Species {
+    constructor(name, group, status, lastSeen, imgUrl, imgCredit)
+    {
+      this.name = name;
+      this.group = group
+      this.status = status
+      this.lastSeen = lastSeen;
+      this.imgUrl = imgUrl;
+      this.imgCredit = imgCredit;
+    }
+  
+    /*
+     - Returns 0 if names are equal
+     - Returns 1 if object calling function is greater than param alphabetically
+     - Returns -1 if object calling function is less than param alphabetically
+    */
+    comp (name){
+      if(this.name == name)
+        return 0;
+      if(this.name > name)
+        return 1;
+      if(this.name < name)
+        return -1;
+    }
+  }
+  
+  /*
+   - Processes list of specie data in 'ESpecies.txt' 
+  */
+  function getSpecies() {
+    let xhr = new XMLHttpRequest();
+    url = "./ESpecies.txt"
+    
+    xhr.open('GET', url, true);
+    xhr.responseType = 'text';
+    
+    xhr.onload = function () {
+      if (xhr.status === 200){
+        const animals = xhr.responseText.split('\n');
+        console.log(animals[0]);
+    
+        for(let i = 0; i < animals.length; i++){
+          // input animal data into species class
+        }
+        
+      } else {
+        // Handle errors
+        console.error('Failed to load CSV file. Status:', xhr.status);
+      }
+      };
+    xhr.send();
+  }
+
+const FRESH_PRINCE_URL = "https://cdn.mos.cms.futurecdn.net/V5JyoDnQWRf2ootiWDQmFc-1200-80.jpg.webp";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
